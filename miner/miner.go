c66e80efd9b87ff688327b2261807ca91a346483
cygaar <tmpftl1@gmail.com>
2023-12-31 00:24:15 -0600

    Double max Ethereum block gas limit

--- miner/miner.go
+++ miner/miner.go
@@ -57,7 +57,7 @@ type Config struct {
 
 // DefaultConfig contains default settings for miner.
 var DefaultConfig = Config{
-	GasCeil:  30000000,
+	GasCeil:  60000000,
 	GasPrice: big.NewInt(params.GWei),
 
 	// The default recommit time is chosen as two seconds since
